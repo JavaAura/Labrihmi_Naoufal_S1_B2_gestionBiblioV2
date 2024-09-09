2a275e8a216e6de6c687aa8bec90cfbef8b81f20
Naoufal <naoufal.labrihmi@gmail.com>
2024-09-09 21:05:16 +0100

    Create TheseUniversitaire.java

---
+++ src/essentiel/doc/TheseUniversitaire.java
@@ -0,0 +1,26 @@
+package essentiel.doc;
+
+import java.time.LocalDate;
+
+public class TheseUniversitaire extends Document {
+    private String university;
+
+    public TheseUniversitaire(int id, String titre, String auteur, LocalDate datePublication, int nombreDePages,
+            String university) {
+        super(id, titre, auteur, datePublication, nombreDePages);
+        this.university = university;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(String university) {
+        this.university = university;
+    }
+
+    @Override
+    public void afficherDetails() {
+        System.out.println("Thèse Universitaire: " + getTitre() + " by " + getAuteur() + " from " + university);
+    }
+}
