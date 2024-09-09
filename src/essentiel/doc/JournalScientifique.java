027fe5bc8d4840a09ad0f854adc104ce48955fa1
Naoufal <naoufal.labrihmi@gmail.com>
2024-09-09 21:05:23 +0100

    Create JournalScientifique.java

---
+++ src/essentiel/doc/JournalScientifique.java
@@ -0,0 +1,26 @@
+package essentiel.doc;
+
+import java.time.LocalDate;
+
+public class JournalScientifique extends Document {
+    private String domaineRecherche;
+
+    public JournalScientifique(int id, String titre, String auteur, LocalDate datePublication, int nombreDePages,
+            String domaineRecherche) {
+        super(id, titre, auteur, datePublication, nombreDePages);
+        this.domaineRecherche = domaineRecherche;
+    }
+
+    public String getDomaineRecherche() {
+        return domaineRecherche;
+    }
+
+    public void setDomaineRecherche(String domaineRecherche) {
+        this.domaineRecherche = domaineRecherche;
+    }
+
+    @Override
+    public void afficherDetails() {
+        System.out.println("Journal Scientifique: " + getTitre() + " by " + getAuteur() + " in " + domaineRecherche);
+    }
+}
