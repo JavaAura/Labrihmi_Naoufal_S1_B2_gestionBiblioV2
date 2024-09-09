6eb8b917734b6745287b1d00d4fe56e1e442f050
Naoufal <naoufal.labrihmi@gmail.com>
2024-09-09 23:27:46 +0100

    Update TheseUniversitaire.java

--- src/essentiel/doc/TheseUniversitaire.java
+++ src/essentiel/doc/TheseUniversitaire.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 public class TheseUniversitaire extends Document {
     private String university;
 
-    public TheseUniversitaire(int id, String titre, String auteur, LocalDate datePublication, int nombreDePages,
+    public TheseUniversitaire(String id, String titre, String auteur, LocalDate datePublication, int nombreDePages,
             String university) {
         super(id, titre, auteur, datePublication, nombreDePages);
         this.university = university;
