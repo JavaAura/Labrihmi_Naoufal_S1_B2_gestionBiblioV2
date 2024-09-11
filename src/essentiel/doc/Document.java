cbd27028bf1504a5e641e2cddcec2cce2ad1808b
Naoufal <naoufal.labrihmi@gmail.com>
2024-09-11 14:58:50 +0100

    Update Document.java

--- src/essentiel/doc/Document.java
+++ src/essentiel/doc/Document.java
@@ -1,22 +1,30 @@
 package essentiel.doc;
 
 import java.time.LocalDate;
+import essentiel.Empruntable;
+import essentiel.Reservable;
 
-public abstract class Document {
-    private String id;
-    private String titre;
-    private String auteur;
-    private LocalDate datePublication;
-    private int nombreDePages;
+public abstract class Document implements Empruntable, Reservable {
+    protected String id;
+    protected String titre;
+    protected String auteur;
+    protected LocalDate datePublication;
+    protected int nombreDePages;
+    protected boolean emprunter;
+    protected boolean reservation;
 
-    public Document(String id, String titre, String auteur, LocalDate datePublication, int nombreDePages) {
+    public Document(String id, String titre, String auteur, LocalDate datePublication, int nombreDePages,
+            boolean emprunter, boolean reservation) {
         this.id = id;
         this.titre = titre;
         this.auteur = auteur;
         this.datePublication = datePublication;
         this.nombreDePages = nombreDePages;
+        this.emprunter = emprunter;
+        this.reservation = reservation;
     }
 
+    // Getters and Setters
     public String getId() {
         return id;
     }
@@ -57,5 +65,61 @@ public abstract class Document {
         this.nombreDePages = nombreDePages;
     }
 
+    public boolean isEmprunter() {
+        return emprunter;
+    }
+
+    public void setEmprunter(boolean emprunter) {
+        this.emprunter = emprunter;
+    }
+
+    public boolean isReservation() {
+        return reservation;
+    }
+
+    public void setReservation(boolean reservation) {
+        this.reservation = reservation;
+    }
+
     public abstract void afficherDetails();
+
+    @Override
+    public void emprunter() {
+        if (!this.emprunter) {
+            this.emprunter = true;
+            System.out.println("Document " + this.titre + " has been borrowed.");
+        } else {
+            System.out.println("Document " + this.titre + " is already borrowed.");
+        }
+    }
+
+    @Override
+    public void retourner() {
+        if (this.emprunter) {
+            this.emprunter = false;
+            System.out.println("Document " + this.titre + " has been returned.");
+        } else {
+            System.out.println("Document " + this.titre + " was not borrowed.");
+        }
+    }
+
+    @Override
+    public void reserver() {
+        if (!this.reservation) {
+            this.reservation = true;
+            System.out.println("Document " + this.titre + " has been reserved.");
+        } else {
+            System.out.println("Document " + this.titre + " is already reserved.");
+        }
+    }
+
+    @Override
+    public void annulerReservation() {
+        if (this.reservation) {
+            this.reservation = false;
+            System.out.println("Reservation for document " + this.titre + " has been canceled.");
+        } else {
+            System.out.println("Document " + this.titre + " was not reserved.");
+        }
+    }
 }
