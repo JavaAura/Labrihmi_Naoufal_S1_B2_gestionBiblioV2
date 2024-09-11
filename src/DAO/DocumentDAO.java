0d971cc6946e4b2b4937292fe4ae30ba6280a1dd
Naoufal <naoufal.labrihmi@gmail.com>
2024-09-11 14:58:25 +0100

    Update DocumentDAO.java

--- src/DAO/DocumentDAO.java
+++ src/DAO/DocumentDAO.java
@@ -2,7 +2,6 @@ package DAO;
 
 import java.sql.SQLException;
 import java.util.List;
-
 import essentiel.doc.Document;
 
 public interface DocumentDAO {
@@ -15,4 +14,36 @@ public interface DocumentDAO {
     void delete(String id) throws SQLException;
 
     List<Document> findAll() throws SQLException;
+
+    /**
+     * Mark a document as borrowed.
+     * 
+     * @param id the ID of the document to mark as borrowed
+     * @throws SQLException if a database access error occurs
+     */
+    void emprunter(String id) throws SQLException;
+
+    /**
+     * Mark a document as returned.
+     * 
+     * @param id the ID of the document to mark as returned
+     * @throws SQLException if a database access error occurs
+     */
+    void retourner(String id) throws SQLException;
+
+    /**
+     * Mark a document as reserved.
+     * 
+     * @param id the ID of the document to mark as reserved
+     * @throws SQLException if a database access error occurs
+     */
+    void reserver(String id) throws SQLException;
+
+    /**
+     * Cancel the reservation of a document.
+     * 
+     * @param id the ID of the document to cancel the reservation
+     * @throws SQLException if a database access error occurs
+     */
+    void annulerReservation(String id) throws SQLException;
 }
